543b72d60a5257134d761f84060bfd67e41d2ae4
wo2168 <71407594+wo2168@users.noreply.github.com>
2021-12-17 16:45:18 -0500

    edited transformation function

--- mx.c
+++ mx.c
@@ -15,18 +15,18 @@ static void die(const char *message)
 typedef struct Matrix {
   int num_rows;
   int num_cols;
-  int* matrixAddr; // accessed [row][col]
+  int *matrixAddr; // accessed [row][col]
   int buildPosition;
 } Matrix;
 // typedef struct matrix matrix;
 
 
-// Matrix* initMatrix(int a){
+// Matrix *initMatrix(int a){
 //   printf("%i\n", a);
 //   printf("LMAOOOO \n");
 // };
 
-Matrix* store(Matrix *m, int value){  
+Matrix *store(Matrix *m, int value){  
   int position = m->buildPosition;
   int curr_row = position / m->num_cols;
   int curr_col = position % m->num_cols;
@@ -36,7 +36,7 @@ Matrix* store(Matrix *m, int value){
   return m;
 }
 
-Matrix* initMatrix( int rows, int cols ) {
+Matrix *initMatrix( int rows, int cols ) {
 
   // printf("COLS + ROWS");
   // printf("ROWS %d",rows);
@@ -49,7 +49,7 @@ Matrix* initMatrix( int rows, int cols ) {
     p[i] = 0;
   }
 
-  Matrix* result = malloc(sizeof(struct Matrix));
+  Matrix *result = malloc(sizeof(struct Matrix));
   result->num_cols = cols;
   result->num_rows = rows;
   result->matrixAddr = p;
@@ -58,43 +58,43 @@ Matrix* initMatrix( int rows, int cols ) {
   return result;
 }
 
-int get(struct Matrix* m, int r,int c){
+int get(struct Matrix *m, int r, int c){
   //get m[r][c]
   int kill = 0;
-  if (r>((m->num_rows)-1)){
+  if (r > ((m->num_rows) - 1)){
     perror("row index out of range when setting matrix ");
     kill = 1;
   }
-  if (c>((m->num_cols)-1)){
+  if (c > ((m->num_cols) - 1)){
     perror("col index out of range when setting matrix ");
     kill = 1;
   }
-  if(kill==1){
+  if(kill == 1){
     die("");
   }
   int idx = c + (r * (m->num_cols));
   return m->matrixAddr[idx];
 }
 
-void set( struct Matrix* m,int r,int c,double v){
+void set(struct Matrix *m, int r, int c, double v){
   //set m[r][c] to v
   int kill = 0;
-  if (r>((m->num_rows)-1)){
+  if (r > ((m->num_rows) - 1)){
     perror("row index out of range when setting matrix ");
     kill = 1;
   }
-  if (c>((m->num_cols)-1)){
+  if (c > ((m->num_cols) - 1)){
     perror("col index out of range when setting matrix ");
     kill = 1;
   }
-  if(kill==1){
+  if(kill == 1){
     die("");
   }
   int idx = c + (r * (m->num_cols));
   m->matrixAddr[idx]=v;
 }
 
-Matrix* mxAdd(Matrix* lhs, Matrix* rhs) {
+Matrix *mxAdd(Matrix *lhs, Matrix *rhs) {
   //check dimensions
   if (lhs->num_rows != rhs->num_rows || lhs->num_cols != rhs->num_cols) {
     perror("Addition size mismatch.");
@@ -102,19 +102,17 @@ Matrix* mxAdd(Matrix* lhs, Matrix* rhs) {
   int rows = lhs->num_rows;
   int cols= lhs->num_cols;
   Matrix *result = initMatrix(rows, cols);
-  for(int i=0; i < rows; i++) {
-    for(int j=0; j < cols; j++) {
-        int sum = get(lhs,i,j)+get(rhs,i,j);
-        set(result,i,j,sum);
+  for(int i = 0; i < rows; i++) {
+    for(int j = 0; j < cols; j++) {
+        int sum = get(lhs, i, j) + get(rhs, i, j);
+        set(result, i, j, sum);
     }
   }
-
   return result;
 }
 
 Matrix *mxSub(Matrix *lhs, Matrix *rhs) 
 {
-
   //check dimensions
   if (lhs->num_rows != rhs->num_rows || lhs->num_cols != rhs->num_cols) {
     perror("Subtraction size mismatch.");
@@ -122,10 +120,10 @@ Matrix *mxSub(Matrix *lhs, Matrix *rhs)
   int rows = lhs->num_rows;
   int cols = lhs->num_cols;
   Matrix *result = initMatrix(rows, cols);
-  for(int i=0; i < rows; i++) {
+  for(int i = 0; i < rows; i++) {
     for(int j = 0; j < cols; j++) {
-        int res = get(lhs,i,j)-get(rhs,i,j);
-        set(result,i,j,res);
+        int res = get(lhs, i, j) - get(rhs, i, j);
+        set(result, i, j, res);
     }
   }
   return result;
@@ -148,7 +146,6 @@ Matrix *mxMult(Matrix *lhs, Matrix *rhs)
     	}
     }
   }
-
   return result;
 }
 
@@ -170,47 +167,9 @@ Matrix *identity(int dim)
 {
   int rows = dim;
   int cols = dim;
-  Matrix *id = initMatrix(rows, cols);
+  Matrix *result = initMatrix(rows, cols);
   for(int i = 0; i < rows; i++) {
     for(int j = 0 ; j < cols; j++) {
-      if(i == j) {
-        set(id, i, j, 1);
-      } else {
-        set(id, i, j, 0);
-      }
-    }
-  }
-  return id;
-}
-
-Matrix *rotation2D(double theta)
-{
-  //translate angle into radians
-  const double pi = 4. * atan(1.);
-  double rads = (theta * pi) / 180;
-  //create rotation matrix
-  Matrix *result = initMatrix(2, 2);
-  set(result, 0, 0, cos(rads));
-  set(result, 0, 1, -sin(rads));
-  set(result, 1, 0, sin(rads));
-  set(result, 1, 1, cos(rads));
-  
-  return result;
-}
-
-Matrix *rotation3D(double theta)
-{
-  //translate angle into radians
-  const double pi = 4. * atan(1.);
-  double rads = (theta * pi) / 180;
-  //create rotation matrix
-  Matrix *result = initMatrix(3, 3);
-  set(result, 0, 0, cos(rads));
-  set(result, 0, 1, -sin(rads));
-  set(result, 1, 0, sin(rads));
-  set(result, 1, 1, cos(rads));
-  for(int i = 2; i < 3; i++) {
-    for(int j = 2; j < 3; j++) {
       if(i == j) {
         set(result, i, j, 1);
       } else {
@@ -221,35 +180,107 @@ Matrix *rotation3D(double theta)
   return result;
 }
 
-Matrix* transpose(Matrix* input) {
+Matrix *transpose(Matrix *input) {
   //switch rows and cols, get empty(i.e., zeroed matrix of transposed size, then fill)
   int rows = input->num_rows;
   int cols = input->num_cols;
 
   Matrix *result = initMatrix(cols, rows);
-  for(int i=0; i<rows; i++) {
-    for(int j=0; j<cols; j++) {
-        set(result, j,i, get(input,i,j));
+  for(int i = 0; i < rows; i++) {
+    for(int j = 0; j < cols; j++) {
+        set(result, j, i, get(input,i,j));
     }
   }
   return result;
 }
 
-void display(Matrix* input) {
-    int row = input->num_rows;
-    int col = input->num_cols;
-    for(int i = 0; i<row; i++) {
-        for(int j=0; j<col; j++) {
-          if (j == 0) {
-            printf("[ %i,", get(input,i,j));
-          } else if (j == col - 1) {
-            printf(" %i ]", get(input,i,j));
-          } else {
-          printf(" %i,", get(input,i,j));
-          }
-        }
-        printf("\n");
+Matrix *transformation(Matrix *input, int num) {
+  //check dimensions
+  if(input->num_rows != 2) {
+    perror("Invalid matrix size for 2D transformations");
+  int rows = input->num_rows;
+  int cols = input->num_cols;
+  Matrix *result = initMatrix(rows, cols);
+  Matrix *tmp = initMatrix(2, 2);
+  switch(num) {
+    case 1 :
+      printf("Reflection in line y = x:\n");
+      set(tmp, 0, 0, 0);
+      set(tmp, 0, 1, 1);
+      set(tmp, 1, 0, 1);
+      set(tmp, 1, 1, 0);
+      result = mxMult(tmp, input);      
+      break;
+    case 2 :
+      printf("Reflection in line y = -x:\n");
+      set(tmp, 0, 0, 0);
+      set(tmp, 0, 1, -1);
+      set(tmp, 1, 0, -1);
+      set(tmp, 1, 1, 0);
+      result = mxMult(tmp, input);
+      break;
+    case 3 :
+      printf("Reflection in x-axis:\n");
+      set(tmp, 0, 0, 1);
+      set(tmp, 0, 1, 0);
+      set(tmp, 1, 0, 0);
+      set(tmp, 1, 1, -1);
+      result = mxMult(tmp, input);
+      break;
+    case 4 :
+      printf("Reflection in y-axis:\n");
+      set(tmp, 0, 0, -1);
+      set(tmp, 0, 1, 0);
+      set(tmp, 1, 0, 0);
+      set(tmp, 1, 1, 1);
+      result = mxMult(tmp, input);
+      break;
+    case 5 :
+      printf("Rotation 90° clockwise:\n");
+      set(tmp, 0, 0, 0);
+      set(tmp, 0, 1, 1);
+      set(tmp, 1, 0, -1);
+      set(tmp, 1, 1, 0);
+      result = mxMult(tmp, input);
+      break;
+    case 6 :
+      printf("Rotation 180°:\n");
+      set(tmp, 0, 0, -1);
+      set(tmp, 0, 1, 0);
+      set(tmp, 1, 0, 0);
+      set(tmp, 1, 1, -1);
+      result = mxMult(tmp, input);
+      break;
+    case 7 :
+      printf("Rotation 90° anticlockwise:\n");
+      set(tmp, 0, 0, 0);
+      set(tmp, 0, 1, -1);
+      set(tmp, 1, 0, 1);
+      set(tmp, 1, 1, 0);
+      result = mxMult(tmp, input);
+      break;
+    default :
+      printf("Invalid input.");
+  }
+  return result;
+}
+
+void display(Matrix *input) {
+  int row = input->num_rows;
+  int col = input->num_cols;
+  printf("\n");
+  for(int i = 0; i < row; i++) {
+    for(int j = 0; j < col; j++) {
+      if (j == 0) {
+        printf("[ %i,", get(input, i, j));
+      } else if (j == col - 1) {
+        printf(" %i ]", get(input, i, j));
+      } else {
+        printf(" %i,", get(input, i, j));
+      }
     }
+    printf("\n");
+  }
 }
 
 void twoFunc(int a, int b){
