4b66fbd33a337b42ad1502188e411b9723710909
54aaron <arj2145@columbia.edu>
2021-12-18 14:49:43 -0500

    it works :<

--- run4.sh
+++ run4.sh
@@ -2,10 +2,10 @@
 
 IN=$1
 arrIN=(${IN//./ })
-echo ${arrIN[0]}
+PROGRAM = ${arrIN[0]}
 
-# make
-# ./mx.native "$1.mx" > "$1.ll"
-# llc -relocation-model=pic "$1.ll" > "$1.s"
-# cc -o "$1.exe" "$1.s" mx.o -lm
-# "./$1.exe"
+make
+./mx.native "${arrIN[0]}.mx" > "${arrIN[0]}.ll"
+llc -relocation-model=pic "${arrIN[0]}.ll" > "${arrIN[0]}.s"
+cc -o "${arrIN[0]}.exe" "${arrIN[0]}.s" mx.o -lm
+"./${arrIN[0]}.exe"
