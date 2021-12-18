85e9b0cb5234063fc77a7605cfb0f8019a222a01
54aaron <arj2145@columbia.edu>
2021-12-18 14:46:23 -0500

    developing general compilation script

--- run4.sh
+++ run4.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-make
-./mx.native "$1.mx" > "$1.ll"
-llc -relocation-model=pic "$1.ll" > "$1.s"
-cc -o "$1.exe" "$1.s" mx.o -lm
-"./$1.exe"
+IN=$1
+arrIN=(${IN//./ })
+echo ${arrIN[0]}
+
+# make
+# ./mx.native "$1.mx" > "$1.ll"
+# llc -relocation-model=pic "$1.ll" > "$1.s"
+# cc -o "$1.exe" "$1.s" mx.o -lm
+# "./$1.exe"
